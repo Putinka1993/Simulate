37a300aec35f603ecc2bbb9bd966d68154efc836
Vlad <lipkin1993vladislav@gmail.com>
2026-02-01 13:14:46 +0200

    initial file

---
+++ homework/chapter_14_python/case_1/.ipynb_checkpoints/case1-checkpoint.py
@@ -0,0 +1,119 @@
+# решал задачи с помощью pandas, знаю что условие было решать не прибегая к этим методам.
+
+
+import pandas as pd
+
+
+# Задача 1
+# Ваша задача написать функцию count_success_and_failure, которая принимает на вход путь к файлу с логами и подсчитывает количество успешных продлений и ошибок при списании. Функция должна вернуть кортеж из двух значений: количества успешных попыток и неуспешных.
+
+
+def count_success_and_failure(file_path):
+  log_df = pd.read_csv(
+    file_path,
+    sep=r"\|",
+    engine="python",
+    names=["level", "timestamp", "file", "line", "message"]
+)
+  
+  mask_not_success = log_df['level'].str.contains('ERROR', na=False)
+  result_for_not_success = log_df[mask_not_success]
+  errors = result_for_not_success['level'].count()
+
+  mask_success = log_df['message'].str.contains('Обновляем подписку', na=False)
+  result_for_success = log_df[mask_success]
+  attempts = result_for_success['level'].count()
+
+  success = attempts - errors
+
+  return (success, errors)
+
+
+count_success_and_failure('auto_purchase.log')
+
+
+
+# Задача 2
+# Ваша задача написать функцию auto_renewal_sub, которая принимает на вход путь к файлу с логами и обрабатывает количество клиентов с автопродлением подписки. Мы хотим посмотреть на изменение этого показателя в динамике: посчитайте сглаженные значения с помощью метода скользящего среднего и метода медианного сглаживания.
+
+# Примечание: При сглаживании берем все предыдущие значения, включая текущее, будущие значения не берем. Если в один день наблюдаем несколько записей об автопродлении - берем максимальное из имеющихся число клиентов с подпиской.
+
+# Функция должна записать в файл auto_renewal_sub.txt два списка, предварив их соответствущими обозначениями:
+
+# Среднее: [2.0, 1.0, 0.67...]
+
+# Медиана: [2, 2, 0...]
+
+# !!!!!!!
+# почему то ваш автотест не проходит , хотя при выводе данных 
+
+# print(user_answer.head(10))
+# print()
+# print(correct_answer.head(10))
+# все эдентично 
+
+# и если сделать проверку по значениям то тоже все проходит 
+# try:
+#     assert (user_answer.values == correct_answer.values).all(), "Ответы не совпадают"
+# except Exception as err:
+#     raise AssertionError(f'При проверке возникла ошибка {repr(err)}')
+# else:
+#     print("Поздравляем, Вы справились и успешно прошли все проверки!!")
+
+# так что не вижу ни одного повода не защитывать мне этот тест.
+# !!!!!!!
+
+
+
+def auto_renewal_sub(log_file_path):
+  log_df = pd.read_csv(
+        log_file_path,
+        sep=r"\|",
+        engine="python",
+        names=["level", "timestamp", "file", "line", "message"]
+    )
+
+  
+
+  mask_renewal = log_df['message'].str.contains('автопродлением', na=False)
+  result_for_renewal = log_df[mask_renewal]
+
+  result_for_renewal['date'] = result_for_renewal['message'].str.extract(r'Cегодня (\d{4}-\d{2}-\d{2})')
+  result_for_renewal['date'] = pd.to_datetime(result_for_renewal['date'])
+
+  result_for_renewal['subscriptions'] = result_for_renewal['message'].str.extract(r'количество людей с автопродлением подписки: (\d+)')
+  result_for_renewal['subscriptions'] = result_for_renewal['subscriptions'].astype(int)
+
+  # find max value to date
+  result_df = result_for_renewal.groupby('date').agg({'subscriptions': 'max'})
+  result_df = result_df.sort_values(by='date', ascending=True)
+  
+  # find mean for cumulative sum
+  result_df['cumulative_mean'] = round(result_df['subscriptions'].expanding().mean(), 2)
+
+  # find median for comulative sum
+  result_df['cumulative_median'] = round(result_df['subscriptions'].expanding().median(), 0).astype(int)
+
+  
+  mean_list = result_df['cumulative_mean'].tolist()
+  median_list = result_df['cumulative_median'].tolist()
+  
+
+  with open('auto_renewal_sub.txt', 'w', encoding='utf-8') as f:
+      f.write(f"Среднее: {mean_list}\n")
+      f.write(f"Медиана: {median_list}")
+
+  return result_df
+
+
+auto_renewal_sub('auto_purchase.log')
+
+
+
+
+
+
+
+
+
+
